e3c755f06cbb9730293fe432db2a4becc194bf3a
Wallace Gabriel <gs008963@gmail.com>
2024-04-20 22:41:59 -0300

    feat: Adiciona o servidor node com Express

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const server = express();
+const port = 3000;
+
+server.use(express.static(path.join(__dirname)));
+
+server.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+server.listen(port, () => {
+  console.log("Server is running");
+});
